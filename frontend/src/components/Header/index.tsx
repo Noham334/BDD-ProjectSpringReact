a8f998852de4eda4f85ee958a8103026f5fb0a47
Israel Hurtado <israelmateus04@gmail.com>
2022-11-17 16:43:07 -0400

    Use state

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,19 @@
-import logo from "../../assets/img/logo.svg"
-import './style.css'
+import logo from '../../assets/img/logo.svg';
+import './style.css';
 
 function Header() {
-  return(
-    <header>
-      <div className="dsmeta-logo-container">
-        <img src={logo} alt="DSMeta" />
-        <h1>DSMeta</h1>
-        <p>Desarrollado por <a href="https://www.instagram.com/israel_noham">@israel_noham</a></p>
-      </div>
-    </header>
-  )
+	return (
+		<header>
+			<div className="dsmeta-logo-container">
+				<img src={logo} alt="DSMeta" />
+				<h1>DSMeta</h1>
+				<p>
+					Desarrollado por{' '}
+					<a href="https://www.instagram.com/israel_noham">@israel_noham</a>
+				</p>
+			</div>
+		</header>
+	);
 }
 
-export default Header
+export default Header;
