11ee336c3c95a72416c90a3315959452a4b40a59
Israel Hurtado <israelmateus04@gmail.com>
2022-11-17 16:19:43 -0400

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from "../../assets/img/logo.svg"
+import './style.css'
+
+function Header() {
+  return(
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>Desarrollado por <a href="https://www.instagram.com/israel_noham">@israel_noham</a></p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
