d7828f8c3cc162f878e54ca130d5b03fb78e02ba
Israel Hurtado <israelmateus04@gmail.com>
2022-11-20 12:05:18 -0400

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -63,7 +63,7 @@ function SalesCard() {
 								<tr key={sale.id}>
 									<td className="show992">{sale.id}</td>
 									<td className="show576">
-										{new Date(sale.date).toLocaleDateString}
+										{new Date(sale.date).toLocaleDateString()}
 									</td>
 									<td>{sale.sellerName}</td>
 									<td className="show992">{sale.visited}</td>
